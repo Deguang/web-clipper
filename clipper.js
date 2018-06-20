3d7f113ffb9613682903c6be1d05e1514e596d38
Deguang <better_li@live.com>
2018-06-21 02:14:18 +0800

    performance issue

--- clipper.js
+++ clipper.js
@@ -1,9 +1,14 @@
 // 网页剪藏，输出包含 computedStyle 的 HTML String
 const clipper = (node = document.body) => {
-    traversalUsingTreeWalker(document.body);
-    // var mirrorContainer = node.cloneNode(true);
-    // document.body.appendChild(mirrorContainer)
-    console.log(document.body.innerHTML);
+    var mirrorContainer = node.cloneNode(true);
+    var e = document.createElement("iframe");
+    e.style.width = e.style.height = 0;
+    document.documentElement.appendChild(e);
+
+    traversalUsingTreeWalker(mirrorContainer);
+    console.log(mirrorContainer.outerHTML);
+
+
 }
 
 function traversalUsingTreeWalker(node){
@@ -22,10 +27,25 @@ function traversalUsingTreeWalker(node){
 }
 
 function setInlineStyle(node) {
-    var s = window.getComputedStyle(node, null);
+    var n = {}, r = node && 1 === node.nodeType ? window.getComputedStyle(node, null) : null;
+    // debugger
+    // if (r) {
+    //     for (var i = 0; i < r.length / 2; i++) {
+    //         var o = r.item(i);
+    //         null != o && (n[o] = r.getPropertyValue(o))
+    //     }
+    // }
     // console.log(node.tagName, s)
-    for (var key in s) {
-        var property = key.replace(/\-([a-z])/g, function(v) { return v[1].toUpperCase(); });
-        node.style[property] = s[key];
+    // for (var key in s) {
+    //     var property = key.replace(/\-([a-z])/g, function(v) { return v[1].toUpperCase(); });
+    //     node.style[property] = s[key];
+    // }
+    var t = "";
+    for (var key in r) {
+        console.log(key, r[key])
+        t += key + ":" + r[key] + ";";
     }
+    node.setAttribute("style", t.trim());
 }
+
+clipper(document.getElementById('resume-detail-wrapper'))
