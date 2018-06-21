4f9c8bd26f9f3e947af700e54f92532b7d11475e
Deguang <better_li@live.com>
2018-06-22 00:07:49 +0800

    保存指定元素innerHTML && computed style

--- clipper.js
+++ clipper.js
@@ -1,51 +1,116 @@
 // 网页剪藏，输出包含 computedStyle 的 HTML String
-const clipper = (node = document.body) => {
-    var mirrorContainer = node.cloneNode(true);
-    var e = document.createElement("iframe");
-    e.style.width = e.style.height = 0;
-    document.documentElement.appendChild(e);
+function clipper (node = document.body) {
+    var iframe = document.createElement("iframe");
+    iframe.setAttribute('id', 'mirror-container');
+    iframe.style.width = iframe.style.height = 0;
+    document.documentElement.appendChild(iframe);
+    var mirrorContainer = document.getElementById('mirror-container');
+    setTagANDgetStyle(node);
+    var mirrorNode = node.cloneNode(true);
+    rmNoPrint(mirrorNode, config.rd5.rm);
+    convertImgToBase64(mirrorNode);
+    mirrorContainer.contentDocument.body.appendChild(mirrorNode);
+    filterStyle(mirrorContainer.contentDocument.body);
+    console.log(mirrorContainer.contentDocument.body.innerHTML);
+    document.documentElement.removeChild(iframe);
+};
 
-    traversalUsingTreeWalker(mirrorContainer);
-    console.log(mirrorContainer.outerHTML);
-
-
+function rmNoPrint(container, rm) {
+    var rmList = container.querySelectorAll(rm);
+    rmList.forEach(node => {
+        var pa = node.parentNode;
+        if(pa) {
+            pa.removeChild(node)
+        }
+    })
 }
 
-function traversalUsingTreeWalker(node){
-    var treeWalker = document.createTreeWalker(node, NodeFilter.SHOW_ELEMENT,null,false);
-    console.log('node: ', node);
-    if(node && node.nodeType === 1){
-        console.log('tagName(1): ', node.tagName);
-        setInlineStyle(node);
-    }
-    var node = treeWalker.nextNode();
-    while(node != null){
-        console.log("tagName(!1): ",node.tagName);
-        setInlineStyle(node);
-        node = treeWalker.nextNode();
-    }
+function convertImgToBase64(container) {
+    var imgList = container.querySelectorAll('img');
+    imgList.forEach(node => {
+        toDataURL(node.src, function(dataUri){
+            console.log(dataUri);
+            node.setAttribute('src', dataUri)
+        })
+    })
 }
 
-function setInlineStyle(node) {
+function toDataURL(src, callback) {
+    var x = new XMLHttpRequest();
+    x.open('GET', 'https://cors-anywhere.herokuapp.com/' + src);
+    x.responseType = 'blob';
+    x.onload = function() {
+        var blob = x.response;
+        var fr = new FileReader();
+        fr.onloadend = function() {
+            var dataUrl = fr.result;
+            callback(dataUrl);
+        };
+        fr.readAsDataURL(blob);
+    };
+    x.send();
+}
+
+var CSSList = [],
+    curStyle = void 0;
+
+function setTagANDgetStyle(node) {
+    var n = 0,
+        loop = function loopNode(node) {
+            var style = u(node);
+            CSSList[n] = style, node.setAttribute("data-clipper-id", n++);
+            for (var o = 0; o < node.children.length; o++) {
+                loopNode(node.children.item(o))
+            }
+        };
+    loop(node)
+}
+
+function u(node) {
     var n = {}, r = node && 1 === node.nodeType ? window.getComputedStyle(node, null) : null;
-    // debugger
-    // if (r) {
-    //     for (var i = 0; i < r.length / 2; i++) {
-    //         var o = r.item(i);
-    //         null != o && (n[o] = r.getPropertyValue(o))
-    //     }
-    // }
-    // console.log(node.tagName, s)
-    // for (var key in s) {
-    //     var property = key.replace(/\-([a-z])/g, function(v) { return v[1].toUpperCase(); });
-    //     node.style[property] = s[key];
-    // }
-    var t = "";
-    for (var key in r) {
-        console.log(key, r[key])
-        t += key + ":" + r[key] + ";";
+    if (!r) return n;
+    for (var i = 0; i < r.length / 2; i++) {
+        var o = r.item(i);
+        null != o && (n[o] = r.getPropertyValue(o))
     }
-    node.setAttribute("style", t.trim());
+    return n
 }
 
-clipper(document.getElementById('resume-detail-wrapper'))
+function filterStyle(node){
+    for (var n = 0, len = node.children.length; n < len; n ++) {
+
+        var item = node.children.item(n),
+            index = item.getAttribute('data-clipper-id');
+
+        if (null != index) {
+            item.removeAttribute('data-clipper-id');
+            item.removeAttribute('style');
+            var style = CSSList[index],
+            curStyle = curStyle || u(item),
+                cptStyle = l(curStyle, style);
+            if(Object.keys(cptStyle).length) {
+                item.setAttribute('style',((style) =>{
+                    var t = "";
+                    for (var n in style) t += n + ":" + style[n] + ";";
+                    return t.trim()
+                })(cptStyle));
+                var rCptStyle = l(u(item), style);
+                if(Object.keys(rCptStyle).length) {
+                    for (var h in rCptStyle) cptStyle[h] = rCptStyle[h];
+                    item.setAttribute('style',((style) =>{
+                        var t = "";
+                        for (var n in style) t += n + ":" + style[n] + ";";
+                        return t.trim()
+                    })(cptStyle));
+                }
+            }
+            filterStyle(item);
+        }
+    }
+}
+
+function l(e, t) {
+    var n = {};
+    for (var r in t) e[r] !== t[r] && (n[r] = t[r]);
+    return n
+}
